cba402c9278c8c5b61d15be790fc26745b3665e3
victor <idamvictorx1@gmail.com>
2024-11-25 23:28:16 +0100

    added skeleton to the ui

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -10,54 +10,18 @@ import {
 import Category from "./Category";
 import { cn } from "@/lib/utils";
 
-// export function CarouselSize({
-//   items,
-//   count,
-//   className,
-//   showControl,
-// }: {
-//   items: React.ReactNode[];
-//   count: string;
-//   className?: string;
-//   showControl: boolean;
-// }) {
-//   const carouselItemClass = ` basis-1/${count} lg:basis-1/${count}`;
-//   return (
-//     <Carousel
-//       opts={{
-//         align: "start",
-//       }}
-//       className={cn(" w-full ", className)}
-//     >
-//       <CarouselContent className="flex justify-between w-full">
-//         {items.map((item: React.ReactNode, index: number) => {
-//           return (
-//             <CarouselItem key={index} className={carouselItemClass}>
-//               {item}
-//             </CarouselItem>
-//           );
-//         })}
-//       </CarouselContent>
-//       {showControl ? (
-//         <div className="flex items-center gap-5 w-full justify-center mt-5">
-//           <CarouselPrevious className="-bottom-[70px] left-[48%] -translate-x-[100%] border" />
-//           <CarouselNext className="-bottom-[70px] right-[50%] translate-x-[100%] border" />
-//         </div>
-//       ) : null}
-//     </Carousel>
-//   );
-// }
-
 export function CarouselSize({
   items,
   count,
   className,
   showControl,
+  isLoading,
 }: {
   items: React.ReactNode[];
   count: string;
   className?: string;
   showControl: boolean;
+  isLoading: boolean;
 }) {
   const carouselItemClass = `basis-1/${parseInt(count, 10) || 1} lg:basis-1/${
     parseInt(count, 10) || 1
